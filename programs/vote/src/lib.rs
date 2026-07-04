26d2186a0dfb36304126b344de8d981dbe62ee25
zack <zack7825@outlook.com>
2026-07-04 17:07:41 +0800

    init

---
+++ programs/vote/src/lib.rs
@@ -0,0 +1,104 @@
+use anchor_lang::prelude::*;
+
+declare_id!("71DneA16fd9TThE6k1ziJbhYBjyYTppBk2HTDLMJffxC");
+
+#[program]
+pub mod vote {
+    use super::*;
+
+    pub fn initialize_poll(
+        ctx: Context<InitializePoll>,
+        poll_id: u64,
+        poll_name: String,
+        poll_description: String,
+        poll_voting_start: u64,
+        poll_voting_end: u64,
+    ) -> Result<()> {
+        ctx.accounts.poll_account.poll_name = poll_name;
+        ctx.accounts.poll_account.poll_description = poll_description;
+        ctx.accounts.poll_account.poll_voting_start = poll_voting_start;
+        ctx.accounts.poll_account.poll_voting_end = poll_voting_end;
+        msg!("initialized poll: {:?}", ctx.program_id);
+        Ok(())
+    }
+
+    pub fn initialize_candidate(
+        ctx: Context<InitializeCandidate>,
+        _poll_id: u64,
+        candidate: String,
+    ) -> Result<()> {
+        ctx.accounts.candidate_account.candidate_name = candidate;
+        ctx.accounts.poll_account.poll_option_index += 1;
+        Ok(())
+    }
+}
+
+
+
+#[derive(Accounts)]
+#[instruction(poll_id: u64)]
+pub struct InitializePoll<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init_if_needed,
+        payer = signer,
+        space = 8 + PollAccount::INIT_SPACE,
+        seeds = [b"poll".as_ref(), poll_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub poll_account: Account<'info, PollAccount>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(poll_id: u64, candidate: String)]
+pub struct InitializeCandidate<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"poll".as_ref(), poll_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub poll_account: Account<'info, PollAccount>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + CandidateAccount::INIT_SPACE,
+        seeds = [poll_id.to_le_bytes().as_ref(), candidate.as_ref()],
+        bump
+    )]
+    pub candidate_account: Account<'info, CandidateAccount>,
+    pub system_program: Program<'info, System>,
+}
+#[account]
+#[derive(InitSpace)]
+pub struct CandidateAccount {
+    #[max_len(32)]
+    pub candidate_name: String,
+    pub candidate_votes: u64,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct PollAccount {
+    #[max_len(32)]
+    pub poll_name: String,
+    #[max_len(280)]
+    pub poll_description: String,
+    pub poll_voting_start: u64,
+    pub poll_voting_end: u64,
+    pub poll_option_index: u64,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("voting is not started")]
+    VotingNotStarted,
+    #[msg("voting has ended")]
+    VotingEnded,
+
+}
